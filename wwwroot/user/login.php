de7f12498e90177d1fee1ac35b4a37c85095562d
husbankhalid <husban.khalid@gmail.com>
2020-03-11 18:53:54 +0530

    Update Login File

--- wwwroot/user/login.php
+++ wwwroot/user/login.php
@@ -6,20 +6,8 @@
     $LoginEmailID = $_POST['LoginEmailID'];
     $LoginPassword = $_POST['LoginPassword'];
 
-    // Experiment
     $HashedPassword = get_login_password($AccountType, $LoginEmailID);
     validate_login_details($AccountType, $LoginEmailID, $LoginPassword, $HashedPassword);
-    // End of Experiment
-
-    // $result = validate_login_details($AccountType, $LoginEmailID, $LoginPassword);
-
-    // if($result == 1) {
-    //   log_in_user($AccountType, $LoginEmailID);
-    //   redirect_to(url_for('/user/' .$AccountType. '/dashboard.php'));
-    // } else {
-    //   $_SESSION['LoginStatus'] = 0;
-    //   redirect_to(url_for('/index.php#login'));
-    // }
 
   } else {
       redirect_to(url_for('/index.php'));
